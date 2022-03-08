61a8285d4b2b9d61440e31ce839d1f3f42d01d8f
deimar <deimar.parn@khk.ee>
2022-03-08 13:40:16 +0000

    "Login" function with different errors"

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-var attempt = 3; // Variable to count number of attempts.
+
 // Below function Executes on click of login button.
+
+$nameErr = $emailErr = $usernameErr = $passwordErr = $DateOfBirthErr = $departmentErr = $ageErr = "";
+
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+
+
     if ( username == "user1" && password == "pass1"){
         alert ("Login successfully");
         window.location = "Dashboard.html"; // Redirecting to other page.
         return false;
     }
+    if(username == "") {
+        document.getElementById("error2").innerHTML
+            = ("Error: Username cannot be blank!");
+
+    }
+
+        if (password == "") {
+            document.getElementById("error2").innerHTML
+                = ("Error: Password cannot be blank!");
+        }
+
+
     else{
-        attempt --;// Decrementing by one.
-        alert("You have left "+attempt+" attempt;");
+
+            document.getElementById("error").innerHTML
+                = "Wrong username or password!";
+
+
 // Disabling fields after 3 attempts.
         if( attempt == 0){
             document.getElementById("username").disabled = true;
@@ -18,5 +38,12 @@ function validate(){
             document.getElementById("submit").disabled = true;
             return false;
         }
+
+
+        }
+
     }
-}
+
+
+
+
